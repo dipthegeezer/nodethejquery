e3c0ca36eed0e9720cdc7fe97a1b3232052610a2
Dipesh Patel <dipthegeezer@opensource.gmail.com>
2012-03-10 15:49:36 +0000

    fixes

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 wget https://raw.github.com/coolaj86/node-jquery/master/src/header.js
 wget http://code.jquery.com/jquery-1.7.1.js
 wget https://raw.github.com/coolaj86/node-jquery/master/src/footer.js
-cat header.js jquery-1.7.1.js footer.js >nodethejson.js
+cat header.js jquery-1.7.1.js footer.js >nodethejquery.js
 rm header.js jquery-1.7.1.js footer.js
